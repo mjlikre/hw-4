464b8e749bae5d5f7c030235eddf29e5db93c3e8
Michael's <mjlikre@gmail.com>
2019-05-25 02:45:31 -0700

    game is basically done, need to fix a bug: when user chooses a new enemy, the new enemy disappears after the first attack. it seems like the system did not update the old enemyLifePoints

--- assets/logic.js
+++ assets/logic.js
@@ -1,30 +1,75 @@
 $(document).ready(function() {
-    var charHp = [100,120,140,160,180,200];
-    var attackPower = 0; 
-    var counterAttack = 0;
-    var isCharChosen = true;
-    var isEnemyChosen = true;
+    let charHp = [100,120,140,160,180,200];
+    let attackPower = 0; 
+    let counterAttackRange = [5,10,15,20];
+    let counterAttack = 0;
+    let isCharChosen = true;
+    let isEnemyChosen = true;
+    $('.playerStatus').hide();
+    $('.enemyStatus').hide();
+    
+
+    
+
 
         $('.charToChoose').on('click', function(){
             if (isCharChosen){
                 $(this).detach().appendTo('#chosenOne').removeClass('charToChoose').addClass("chosenOne");
+                let rand = Math.floor(Math.random()*6);
+                let lifePoints = charHp[rand];
+                var initialAttackPower= (Math.ceil(Math.random()*10)+5);
+                attackPower = initialAttackPower;
+                $(".life-points").html(lifePoints);
+                $('.attack').html(attackPower);
+
+                $('.playerStatus').show();
                 $(".choice-1").hide();
                 $(".charToChoose").detach().appendTo('.enemies').removeClass('charToChoose').addClass('enemyChar');
-                var rand = Math.floor(Math.random()*6);
-                var lifePoints = charHp[rand];
-                $("#life-points").html(lifePoints);
+                
                 isCharChosen = false;
                 
                 $('.enemyChar').on('click', function(){
                     if (isEnemyChosen){
+                        $('.enemyStatus').show();
                         $(this).detach().appendTo(".defender").removeClass('enemyChar').addClass('enemyChosen');
-                        var enRand = Math.floor(Math.random()*6);
-                        var enemyLifePoints = charHp[enRand];
-                        $('#life-points').html(enemyLifePoints);
+                        let enRand = Math.floor(Math.random()*6);
+                        let enemyLifePoints = charHp[enRand];
+                        $('.enemy-life').html(enemyLifePoints);
+                        let moreRand = Math.floor(Math.random()*4);
+                        let counterAttack = counterAttackRange[moreRand];
+                        $('.counter').html(counterAttack);
                         isEnemyChosen = false; 
+                        $('#attacking').on('click', function(){
+                            enemyLifePoints-=attackPower;
+                            $('.enemy-life').html(enemyLifePoints);
+                            lifePoints -= counterAttack;
+                            $(".life-points").html(lifePoints);
+
+                            if (lifePoints > 0){
+                                
+                                attackPower += initialAttackPower;
+                                $('.attack').html(attackPower);
+
+                            }
+                            else{
+                                alert("You Lost!! Refresh To Restard The Game")
+                            } 
+                            
+                            if (enemyLifePoints<=0){
+                                $('.enemyChosen').detach();
+                                isEnemyChosen = true;
+                            }
+                             
+                        });
+                        
+        
                     }
                 });
 
+                
+                
+
+                
                 
                 
             }
